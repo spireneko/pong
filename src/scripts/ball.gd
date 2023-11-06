4bf1ea018128980bd2d00d54700da98b9a2f68c3
spireneko <spireneko@gmail.com>
2023-11-06 10:01:17 +0400

    ball is bouncing and not damping

--- src/scripts/ball.gd
+++ src/scripts/ball.gd
@@ -1,13 +1,18 @@
 extends RigidBody2D
 
+const SPEED := 750
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_central_impulse(Vector2(1, 0) * 1000)
+	linear_damp_mode = RigidBody2D.DAMP_MODE_REPLACE
+	linear_damp = 0
+	apply_central_impulse(
+		Vector2.LEFT.rotated(
+			randf_range(deg_to_rad(-45), deg_to_rad(45))) * SPEED)
 
-func _physics_process(delta: float) -> void:
-	pass
+func apply_bounce_velocity(velocity: Vector2):
+	linear_velocity = (linear_velocity.normalized() + velocity * 0.2).normalized() * SPEED
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _physics_process(delta):
+#	print(linear_velocity)
+#	print(linear_velocity.length())
--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,15 +1,18 @@
-extends StaticBody2D
+extends RigidBody2D
 
 const SPEED := 500
 
 var velocity : Vector2
+var starting_x_pos : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	starting_x_pos = position.x
 	pass # Replace with function body.
 
 
 func _physics_process(delta: float) -> void:
-	move_and_collide(velocity * delta * SPEED)
+#	position.x = starting_x_pos
+	move_and_collide(velocity * SPEED * delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +20,8 @@ func _process(delta: float) -> void:
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	velocity = Vector2(0, Input.get_axis("ui_up", "ui_down"))
+
+
+func _on_body_entered(body):
+	if body is RigidBody2D:
+		body.apply_bounce_velocity(velocity)
